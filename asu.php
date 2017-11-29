498937316eadd07beb4641af64c8028bb38e381b
Berandal <berandal1337@gmail.com>
2017-11-29 12:08:26 +0700

    Add files via upload

---
+++ asu.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang='en' class=''>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+    <style class="cp-pen-styles">
+        body {
+            margin: 0 auto;
+            padding 0px;
+            background: url(http://static.simpledesktops.com/uploads/desktops/2012/02/02/Bombillos.png.625x385_q100.png);
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: 100% 100%;
+            background-attachment: fixed;
+            width: 100%;
+            height: 80%;
+            font-size: 12px;
+            font-family: sans-serif;
+            color: white;
+        }
+        
+        .profile-bg {
+            width: 300px;
+            height: 10%;
+            background-color: transparent;
+            border-style: solid;
+            border-width: 2px 2px 2px 2px;
+            text-align: center;
+            margin: 0 auto;
+            margin-top: 20px;
+            padding: 0px;
+            color: white;
+        }
+        
+        .profile-bg img {
+            width: 250px;
+            overflow: hidden;
+            border-radius: 50%;
+
+        }
+        
+        .social-bg {
+            background-color: transparent;
+            height: 55px;
+            margin: 0 auto;
+            padding: 0px;
+            text-align: center;
+            font-family: 'Press Start 2P', cursive;
+        }
+        
+        .fa {
+            padding: 20px;
+        }
+
+        .content {
+            padding: 30px;
+        }
+        
+        .content p {
+            margin-top: 30px;
+        }
+        
+        h3 {
+            text-transform: uppercase;
+            font-size: 14px;
+            letter-spacing: .2em;
+            font-weight: bold;
+        }
+        
+        small {
+            text-transform: uppercase;
+            font-weight: bold;
+            letter-spacing: .2em;
+            color: #8B9EC7;
+        }
+        
+        footer {
+            text-align: center;
+            text-transform: uppercase;
+            margin: 30px 0px 10px 0px;
+            letter-spacing: .2em;
+            font-size: 10px;
+            font-weight: bold;
+        }
+        
+        a   {
+  text-decoration: none;
+  color: maroon;
+}
+a:hover {
+  text-decoration: none;
+  color: white;
+}
+.btn {
+  cursor: pointer;
+  margin: 10px;
+  border-radius: 5px;
+  text-decoration: none;
+  padding: 10px;
+  font-size: 18px;
+  transition: .3s;
+  -webkit-transition: .3s;
+  -moz-transition: .3s;
+  -o-transition: .3s;
+  display: inline-block;
+}
+
+.btn:hover {
+  cursor: url(http://cur.cursors-4u.net/symbols/sym-1/sym46.cur), auto;
+}
+.red {
+  color: white;
+  border: 2px #808080 solid;
+}
+
+.red:hover {
+  color: white;
+  background-color: ;
+  border: 2px maroon solid;
+}
+    </style>
+</head>
+    <body class="container-fluid">
+        <div class="profile-bg container">
+            <div class="content">
+                <img src="https://giffiles.alphacoders.com/120/120412.gif">
+                <p>
+                    <h3>Berandal | OWL SQUAD</h3> We are gray hats. Not infrequently we help secure a well-intended website. Such as hospitals, police, charities, community organizations and others. We do it without being paid. But we do it honestly, I mean we are frankly asking for their access, because some bugs have to be solved through there. And they gave it. We ask for access to them, they trust us, we give the best for them.
+                </p>
+            </div>
+            <div class="social-bg">
+                <a href="https://facebook.com/owlsquad.id">OWL SQUAD</a>
+                <br>
+                <a href='https://github.com/berandal666'>Github</a>
+                <br>
+                <a href="https://pastebin.com/u/berandal666">Pastebin</a>
+            </div>
+            <a class="btn red" href="mailto:berandal@owl-squad.net">Hire Me!</a>
+        </div>
+        <footer> Designed and Developed by <a href="http://facebook.com/owlsquad.id">Berandal</a>
+        </footer>
+
+    </body>
+
+</html>
